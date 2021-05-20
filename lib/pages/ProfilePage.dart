28576bec2bd50b9e77edfec89e156160ccb27950
nikanlahut <lahutnikan@gmail.com>
2021-05-20 04:38:09 +0300

    updated

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:deneme_app/models/user.dart';
 import 'package:deneme_app/widgets/HeaderWidget.dart';
+import 'package:deneme_app/widgets/PostTileWidget.dart';
+import 'package:deneme_app/widgets/PostWidget.dart';
 import 'package:deneme_app/widgets/ProgressWidget.dart';
 
 import 'package:flutter/material.dart';
@@ -18,7 +21,16 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
   final String currentOnlineUserId = currentUser?.id;
+  bool loading = false;
+  int countPost = 0;
+  List<Post> postsList = [];
+  String postOrientation = "grid";
+
+  void initState(){
+    getAllProfilePosts();
+  }
 
   createProfileTopView(){
     return FutureBuilder(
@@ -48,7 +60,7 @@ class _ProfilePageState extends State<ProfilePage> {
                           mainAxisSize: MainAxisSize.max,
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget> [
-                            createColumns("posts", 0),
+                            createColumns("posts", postsList.length),
                             createColumns("followers", 0),
                             createColumns("following", 0),
                           ],
@@ -150,9 +162,97 @@ class _ProfilePageState extends State<ProfilePage> {
         body: ListView(
             children: <Widget>[
               createProfileTopView(),
-
+              Divider(),
+              createListAndGridPostOrientation(),
+              Divider(height: 0.0,),
+              displayProfilePost(),
             ]
         )
     );
   }
+  displayProfilePost() {
+    if (loading) {
+      return circularProgress();
+    }
+    else if (postsList.isEmpty) {
+      return Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Icon(
+                  Icons.photo_library, color: Colors.grey, size: 200.0,),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text("No Posts", style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),),
+              ),
+            ],
+          )
+      );
+    }
+    else if (postOrientation == "grid") {
+      List<GridTile> gridTilesList = [];
+      postsList.forEach((eachPost){
+        gridTilesList.add(GridTile(child: PostTile(eachPost)));
+      });
+      return GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.5,
+        crossAxisSpacing: 1.5,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: gridTilesList,
+      );
+
+    }
+
+    else if (postOrientation == "list") {
+      return Column(
+        children: postsList,
+      );
+    }
+  }
+  getAllProfilePosts() async{
+    setState(() {
+      loading = true;
+    });
+    QuerySnapshot querySnapshot = await postsReference.doc(widget.userProfileId).collection("usersPosts").orderBy("timestamp", descending: true).get();
+
+    setState(() {
+      loading = false;
+      countPost = querySnapshot.docs.length;
+      postsList = querySnapshot.docs.map((documentSnapshot) => Post.fromDocument(documentSnapshot)).toList();
+    });
+  }
+
+  createListAndGridPostOrientation(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget> [
+        IconButton(
+          onPressed: ()=> setOrientation("grid"),
+          icon: Icon(Icons.grid_on),
+          color: postOrientation == "grid" ? Theme.of(context).primaryColor : Colors.grey,
+        ),
+        IconButton(
+          onPressed: ()=> setOrientation("list"),
+          icon: Icon(Icons.list),
+          color: postOrientation == "list" ? Theme.of(context).primaryColor : Colors.grey,
+        ),
+      ],
+    );
+  }
+  setOrientation(String orientation)
+  {
+    setState(() {
+      this.postOrientation = orientation;
+    });
+  }
 }
+
